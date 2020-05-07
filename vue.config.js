7ab01ea6ee7140c12c7196051eae16cc66b80cf2
piotr-s <piotr.slawinski1@gmail.com>
2020-05-07 19:26:09 +0200

    Yet another clean up

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/wizualizacja-danych-telemetrycznych/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production'
+      ? '/wizualizacja-danych-telemetrycznych/'
+      : '/',
   /*
    * Issue with peer dependencies required to make an alias to point to a single
    * leaflet root instance as in https://github.com/jurb/vue2-leaflet-heatmap/issues/1
